02dcc8bda9c0c43b76a8b6e2cb44feb210c22021
PRIYADHARSHINI-25 <priya35471@gmail.com>
2024-05-14 16:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
         return render_template('home.html')
+@app.route('/login')
+def login():
+        return "Hii"
   
 if __name__ =='__main__':  
     app.run(debug = True)  
