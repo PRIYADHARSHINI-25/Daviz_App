240b03a397de4871a26ae7cf80ae50f597f9312d
PRIYADHARSHINI-25 <priya35471@gmail.com>
2024-05-14 15:28:43 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for, session, render_template, redirect, request,flash
+from authlib.integrations.flask_client import OAuth
+from authlib.integrations.base_client.errors import MismatchingStateError
+from flask_pymongo import MongoClient
+from dotenv import load_dotenv
+import pandas as pd
+import os,gridfs
+from charts import preprocess,chartvis
+
 
 app = Flask(__name__)  
  
-@app.route('/')  
-def home():  
-    return "Hello Saranya";  
+@app.route('/')
+def home():
+        return render_template('home.html')
   
 if __name__ =='__main__':  
     app.run(debug = True)  
